f9f929c2e6e5a61530ee5c458731d1b7440e1aba
Pedro Cristo <pcristo35@gmail.com>
2022-05-31 14:33:20 +0000

    Create super user and models admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Author, Category, Post
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Add fields for Author in admin panel
+    """
+    list_display = ('author', 'timestamp')
+    search_fields = ['author']  
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Add fields which will use summernote editor in admin panel
+    """
+    list_display = ('title', 'slug', 'status', 'timestamp', 'author',)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'timestamp')
+    summernote_fields = ('content')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Add fields for Category in admin panel
+    """
+    list_display = ['title']
+    search_fields = ['title']    
