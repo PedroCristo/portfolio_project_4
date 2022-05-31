12116a93f19e47268bb24c04139594d1a26ebf79
Pedro Cristo <pcristo35@gmail.com>
2022-05-31 12:48:34 +0000

    Change Tasty Blog URL in ALLOWED_HOSTS

--- tastyblog/settings.py
+++ tastyblog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tasty-blog-portfolio-project-4.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['tasty-blog-portfolio-project-4.herokuapp.com', 'localhost']
 
 
 # Application definition
