7aadbf0c7e6e69d3431bbe361b2dd67cfd3d42e0
Pedro Cristo <pcristo35@gmail.com>
2022-06-01 09:28:54 +0000

    Add urls for blog.urls

--- tastyblog/urls.py
+++ tastyblog/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls'), name='blog.urls'),
     path('summernote/', include('django_summernote.urls')),
 ]
