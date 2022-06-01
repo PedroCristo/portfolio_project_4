c7a1788263c59d14bc9b2109ea2468881a831072
Pedro Cristo <pcristo35@gmail.com>
2022-06-01 09:36:53 +0000

    Add views and urls path for index and post detail

---
+++ blog/urls.py
@@ -0,0 +1,19 @@
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from .forms import CommentForm
+
+from .views import *
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+  
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
+from django.views import generic, View
+from posts.models import *
+from .forms import CommentForm
 
-# Create your views here.
+
+
+def index(request):
+    """View to return the index page"""
+    queryset = Post.objects.filter(featured=True
+    ).order_by('-timestamp')
+    categories = Category.objects.all()
+
+    context = {
+        'post_list': queryset,
+        'categories_list': categories,
+    }
+    
+    return render(request, 'index.html', context)
+
+
+class PostDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-timestamp")
+        num_comments = Comment.objects.filter(post=post).count()
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm(),
+                'num_comments': num_comments,
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-timestamp")
+        liked = False
+
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        comment_form = CommentForm(data=request.POST)
+        if  comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked,
+            },
+        )    
