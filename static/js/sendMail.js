e9e2cf3bc2aa54e8717146da3f30f235f7d96220
Pedro Cristo <pcristo35@gmail.com>
2022-06-22 12:57:24 +0000

    Add Emailjs services settings to send emails from the contact form

---
+++ static/js/sendMail.js
@@ -0,0 +1,20 @@
+function sendMail(contactForm) {
+    emailjs.send("service_jui1hv6", "tasty_blog_contact", {
+        "from_name": contactForm.name.value,
+        "surname": contactForm.surname.value,
+        "subject": contactForm.subject.value,
+        "email": contactForm.email.value,
+        "message": contactForm.message.value,
+    })
+    .then(
+        function(response) {
+            document.getElementById('email_alert').innerHTML = `<h4 class="email-sent-message alert-success">Thanks for your email!
+            <br> We will contact you as soon as possible!</h4>`;
+        },
+        function(error) {
+            document.getElementById('email_alert').innerHTML = `<h4 class="email-sent-message  alert-danger">Sorry, something went wrong!
+            <br> Try to send an email again.</h4>`;
+        }
+    );
+    return false;  // To block from loading a new page
+}
