158f8bc0ffb8d491d9ebcc503c1648e0856eb8c9
Pedro Cristo <pcristo35@gmail.com>
2022-06-23 12:01:23 +0000

    Remove extra white space

--- users/apps.py
+++ users/apps.py
@@ -5,6 +5,5 @@ class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
-
     def ready(self):
-        import users.signals    
+        import users.signals
