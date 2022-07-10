399c9f3911b49e1b9c56ef62ef99fbe95e46df62
Pedro Cristo <pcristo35@gmail.com>
2022-07-10 12:12:26 +0000

    Move signals.py to the posts app and remove users' app

--- posts/apps.py
+++ posts/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class PostsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'posts'
+
+    def ready(self):
+        """
+        Gets the user's details
+        """
+        import posts.signals
