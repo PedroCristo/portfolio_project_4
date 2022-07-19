63f8eecdab965590ba0f4fd0a11ec4148b6d513d
Pedro Cristo <pcristo35@gmail.com>
2022-07-19 13:06:35 +0000

    Remove unused code for database

--- tastyblog/settings.py
+++ tastyblog/settings.py
@@ -98,16 +98,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tastyblog.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
